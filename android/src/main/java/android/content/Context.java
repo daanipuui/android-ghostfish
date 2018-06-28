12e63480f3496d05b3e0edce6c707706c518b2f7
Daniel PUIU <danielpuiu@windowslive.com>
2018-06-29 00:48:39 +0300

    removed dependency to android jar as only 3 classes are used

---
+++ android/src/main/java/android/content/Context.java
@@ -0,0 +1,14 @@
+package android.content;
+
+import android.content.res.AssetManager;
+
+public class Context {
+
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    public AssetManager getAssets() {
+        return null;
+    }
+}
---
+++ android/src/main/java/android/content/res/AssetManager.java
@@ -0,0 +1,10 @@
+package android.content.res;
+
+import java.io.InputStream;
+
+public class AssetManager {
+
+    public InputStream open(String filename) {
+        return null;
+    }
+}
